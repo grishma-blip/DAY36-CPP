420565c19f3ef4b135d1ccab95f96398e21d917c
Grishma Thakare <85503530+grishma-blip@users.noreply.github.com>
2024-12-22 13:02:18 +0530

    Add files via upload

---
+++ Cstylestring.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+    char str1[10]="hello";
+    char str2[20]="world";
+    cout<<str1<<"\n";
+    cout<<str2;
+    return 0;
+}
